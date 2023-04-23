ff4b17b95805b8ded0714c39b4f674cc4364406d
mortal123 <lwhao2012@gmail.com>
2023-04-23 18:02:50 +0800

    feat: struct BinaryPath to facilitate path representation

--- zktrie/encoding.go
+++ zktrie/encoding.go
@@ -2,19 +2,39 @@ package zktrie
 
 import itypes "github.com/scroll-tech/zktrie/types"
 
-// binary encoding
-type BinaryPath []bool
+type BinaryPath struct {
+	d    []byte
+	size int
+}
 
-func bytesToPath(b []byte) BinaryPath {
-	panic("not implemented")
+func NewBinaryPathFromKeyBytes(b []byte) *BinaryPath {
+	d := make([]byte, 8*len(b))
+	copy(d, b)
+	return &BinaryPath{
+		size: len(b) * 8,
+		d:    d,
+	}
+}
+
+func (bp *BinaryPath) Size() int {
+	return bp.size
+}
+
+func (bp *BinaryPath) Pos(i int) bool {
+	return (bp.d[i/8] & (1 << (i % 8))) != 0
+}
+
+func (bp *BinaryPath) ToKeyBytes() []byte {
+	if bp.size%8 != 0 {
+		panic("can't convert binary key whose size is not multiple of 8")
+	}
+	d := make([]byte, bp.size)
+	copy(d, bp.d)
+	return d
 }
 
 func bytesToHash(b []byte) *itypes.Hash {
 	var h itypes.Hash
 	copy(h[:], b)
 	return &h
-}
-
-func hashToBytes(hash *itypes.Hash) []byte {
-	return hash[:]
 }
