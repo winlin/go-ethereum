489ac909e57c2b627e42a2f193c90d23afdd5a86
HAOYUatHZ <haoyu@protonmail.com>
2023-08-03 16:55:07 +0800

    fix: fix accRows for empty block

--- miner/worker.go
+++ miner/worker.go
@@ -1223,6 +1223,19 @@ func (w *worker) commitNewWork(interrupt *int32, noempty bool, timestamp int64) 
 // commit runs any post-transaction state modifications, assembles the final block
 // and commits new work if consensus engine is running.
 func (w *worker) commit(uncles []*types.Header, interval func(), update bool, start time.Time) error {
+	// set w.current.accRows for empty block
+	if w.current.accRows == nil || len(*w.current.accRows) == 0 {
+		traces, err := w.current.traceEnv.GetBlockTrace(types.NewBlockWithHeader(w.current.header))
+		if err != nil {
+			return err
+		}
+		accRows, err := w.circuitCapacityChecker.ApplyTransaction(traces)
+		if err != nil {
+			return err
+		}
+		w.current.accRows = accRows
+	}
+
 	// Deep copy receipts here to avoid interaction between different tasks.
 	receipts := copyReceipts(w.current.receipts)
 	s := w.current.state.Copy()
