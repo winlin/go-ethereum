d986c18aec96fa5a9665bbaec4d768b8feda3aa8
HAOYUatHZ <haoyu@protonmail.com>
2023-08-06 23:50:01 +0800

    add test

---
+++ rollup/circuitcapacitychecker/impl_test.go
@@ -0,0 +1,48 @@
+//go:build circuit_capacity_checker
+
+// // in repo_root_dir
+// make libzkp
+// sudo cp ./rollup/circuitcapacitychecker/libzkp/libzkp.so /usr/local/lib/
+// sudo cp ./rollup/circuitcapacitychecker/libzkp/libzktrie.so /usr/local/lib/
+
+// // in this dir
+// go test -v -race -gcflags="-l" -ldflags="-s=false" -tags circuit_capacity_checker ./...
+package circuitcapacitychecker
+
+import (
+    "encoding/json"
+    "flag"
+    "io"
+    "os"
+    "testing"
+
+    "github.com/stretchr/testify/assert"
+    "github.com/scroll-tech/go-ethereum/core/types"
+)
+
+var (
+    tracePath    = flag.String("trace", "/assets/traces/1_transfer.json", "trace")
+)
+
+func TestApplyTx(t *testing.T) {
+    as := assert.New(t)
+    ccc := NewCircuitCapacityChecker()
+
+    trace := readBlockTrace(*tracePath, as)
+
+    _, err := ccc.ApplyTransaction(trace)
+    as.NoError(err)
+}
+
+
+func readBlockTrace(filePat string, as *assert.Assertions) *types.BlockTrace {
+    f, err := os.Open(filePat)
+    as.NoError(err)
+    byt, err := io.ReadAll(f)
+    as.NoError(err)
+
+    trace := &types.BlockTrace{}
+    as.NoError(json.Unmarshal(byt, trace))
+
+    return trace
+}
