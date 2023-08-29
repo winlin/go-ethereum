ec53c9dbf84c2ed3d8d63393fb4e6f449cafd208
HAOYUatHZ <haoyu@protonmail.com>
2023-08-29 13:43:48 +0800

    fix

--- miner/worker.go
+++ miner/worker.go
@@ -234,6 +234,7 @@ func newWorker(config *Config, chainConfig *params.ChainConfig, engine consensus
 		resubmitIntervalCh:     make(chan time.Duration),
 		resubmitAdjustCh:       make(chan *intervalAdjust, resubmitAdjustChanSize),
 		circuitCapacityChecker: circuitcapacitychecker.NewCircuitCapacityChecker(),
+		skippedL2Txs:           make(chan struct{}),
 	}
 	log.Info("created new worker", "CircuitCapacityChecker ID", worker.circuitCapacityChecker.ID)
 
